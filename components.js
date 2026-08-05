5e3b18fb93f09452ab3f7f2468b4fdff14f6601f
j <jacksijl09@gmail.com>
2026-08-05 03:45:50 +0200

    Update components.js

--- components.js
+++ components.js
@@ -4,19 +4,6 @@ function loadComponent(id, file) {
         .then(html => document.getElementById(id).innerHTML = html);
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-    loadComponent("navbar", "navbar.html");
-    loadComponent("footer", "footer.html");
-    loadComponent("hero", "hero.html");
-    loadComponent("marquee", "marquee.html");
-    loadComponent("features", "features.html");
-    loadComponent("bigstats", "big-stats.html");
-    loadComponent("reviews", "reviews.html");
-    loadComponent("faq", "faq-list.html");
-    loadComponent("products", "products.html");
-    loadComponent("about", "about-text.html");
-    loadComponent("contact", "contact-cards.html");
-});
 function appendComponent(id, file) {
     fetch(`/src/components/${file}`)
         .then(res => res.text())
@@ -26,10 +13,35 @@ function appendComponent(id, file) {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+
+    loadComponent("navbar", "navbar.html");
+    loadComponent("footer", "footer.html");
+
+    loadComponent("hero", "hero.html");
+
+    loadComponent("marquee", "marquee.html");
+
     appendComponent("features", "feature-card-01.html");
     appendComponent("features", "feature-card-02.html");
     appendComponent("features", "feature-card-03.html");
     appendComponent("features", "feature-card-04.html");
     appendComponent("features", "feature-card-05.html");
     appendComponent("features", "feature-card-06.html");
+
+    appendComponent("bigstats", "big-stats.html");
+
+    appendComponent("reviews", "review-01.html");
+    appendComponent("reviews", "review-02.html");
+    appendComponent("reviews", "review-03.html");
+
+    loadComponent("faq", "faq-list.html");
+
+    appendComponent("products", "product-card-01.html");
+    appendComponent("products", "product-card-02.html");
+    appendComponent("products", "product-card-03.html");
+
+    loadComponent("about", "about-text.html");
+
+    appendComponent("contact", "contact-card-discord.html");
+    appendComponent("contact", "contact-card-email.html");
 });
